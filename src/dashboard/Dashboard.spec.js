0991c48598fda53dd98a928d454e5e7676e64065
Nathaniel Mosco <natemosco@gmail.com>
2019-11-12 03:33:12 -0500

    default state of unlocked and open -- partial test completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from "react";
+import { render } from "@testing-library/react"
+import Dashboard from "./Dashboard";
+
+test("app renders correctly", () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
+
+test('state is unlocked and open', () => {
+    const dashboard = render(<Dashboard />);
+
+    const lockBtn = dashboard.getByText('Lock Gate');
+    const closeBtn = dashboard.getByText('Close Gate');
+
+    expect(lockBtn.disabled).toBe(true);
+    expect(closeBtn.disabled).toBe(false);
+})
+//BUT... How do you make it based on a snapshot === onLoad?
+
